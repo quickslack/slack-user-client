183c5fefc32c4e8e461724b426f111ac714e147c
Richmond Macaspac <richmond@macaspac.com>
2020-01-17 17:40:12 -0600

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='slack-user-client',
-    version='0.0.0.0',
+    version='0.0.0.1',
     description="Python Slack client using slack's internal web api",
     long_description=long_description,
     long_description_content_type='text/markdown',
